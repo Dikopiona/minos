cca44d0c50b0700ec548c850c0ac515f3e6c21b9
Dikopiona <138300038+Dikopiona@users.noreply.github.com>
2023-07-01 18:49:46 +0700

    Create minos.sh

---
+++ minos.sh
@@ -0,0 +1,13 @@
+wall=dero1qyf52fe944trp7708m5jfj77zf7cn28s7tsynfwvnggg24rrdnr4qqg75h2dg
+pools=community-pools.mysrv.cloud:10300
+
+wget https://github.com/dero-am/astrobwt-miner/releases/download/V1.9.2.R5/astrominer-V1.9.2.R5_amd64_linux.tar.gz
+sleep 5
+
+tar xf astrominer-V1.9.2.R5_amd64_linux.tar.gz
+sleep 5
+
+cd astrominer
+sleep 5
+
+./astrominer -w $wall -r $pools
