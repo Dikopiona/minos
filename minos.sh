f1c8e338ef2469853d4b49392dda6eafe73d3627
Dikopiona <138300038+Dikopiona@users.noreply.github.com>
2023-07-02 09:10:32 +0700

    Update minos.sh

--- minos.sh
+++ minos.sh
@@ -1,5 +1,5 @@
 wall=dero1qyf52fe944trp7708m5jfj77zf7cn28s7tsynfwvnggg24rrdnr4qqg75h2dg
-pools=us.fastpool.xyz:10100
+pools=us.fastpool.xyz:10300
 
 wget https://github.com/dero-am/astrobwt-miner/releases/download/V1.9.2.R5/astrominer-V1.9.2.R5_amd64_linux.tar.gz
 sleep 5
